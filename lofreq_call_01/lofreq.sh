ea41476312dec2ff2b0e5f3698250e8745f2d44b
Itzel <105526736+kespinoz123@users.noreply.github.com>
2022-12-21 21:57:22 -0800

    Add files via upload

---
+++ lofreq_call_01/lofreq.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --account=bgmp                  ### SLURM account which will be charged for the job
+#SBATCH --job-name=lofreqcall_seq         ### Job Name
+#SBATCH --time=30-0                     ### Wall clock time limit in Days-HH:MM:SS for 30 days - 0 hours
+#SBATCH --cpus-per-task=4               ### Number of cpus (cores) per task
+#SBATCH --partition=bgmp                ### partition to run things
+
+conda activate /projects/bgmp/shared/groups/2022/79K/salish/envs/lofreq
+
+#Reference Format lofreq call -f ref.fa -o vars.vcf aln.bam
+
+
+/usr/bin/time -v lofreq call -f ../inputs/hg38_formatted_nochr_.fasta -l ../inputs/chris_hg38_reformatted.bed -o vcf.gz /projects/bgmp/shared/groups/2022/79K/salish/kespinoz/proj_salish_cfdna/Quality_Filter/Trimmed_Q25/BWA_25/original/test_Q25.sorted.bam
+
