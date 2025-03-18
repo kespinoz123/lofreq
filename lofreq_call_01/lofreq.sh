1c32345a39d2df84c0cbed0cf1b8da3546778277
kespinoz123 <kespinoz@uoregon.edu>
2025-03-17 22:01:23 -0700

    lofreq shell script

--- lofreq_call_01/lofreq.sh
+++ lofreq_call_01/lofreq.sh
@@ -6,9 +6,10 @@
 #SBATCH --cpus-per-task=4               ### Number of cpus (cores) per task
 #SBATCH --partition=bgmp                ### partition to run things
 
+# ------- Activating conda environment to run lofreq ------- 
 conda activate /projects/bgmp/shared/groups/2022/79K/salish/envs/lofreq
 
-#Reference Format lofreq call -f ref.fa -o vars.vcf aln.bam
+# ------- Reference Format lofreq call -f ref.fa -o vars.vcf aln.bam  ------- 
 
 
 /usr/bin/time -v lofreq call -f ../inputs/hg38_formatted_nochr_.fasta -l ../inputs/chris_hg38_reformatted.bed -o vcf.gz /projects/bgmp/shared/groups/2022/79K/salish/kespinoz/proj_salish_cfdna/Quality_Filter/Trimmed_Q25/BWA_25/original/test_Q25.sorted.bam
