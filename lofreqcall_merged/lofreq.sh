14ebb51f7504d64ca403d11a936e0d8864c8da6e
kespinoz123 <kespinoz@uoregon.edu>
2025-03-17 21:46:30 -0700

    lofreq shell cript

--- lofreqcall_merged/lofreq.sh
+++ lofreqcall_merged/lofreq.sh
@@ -4,11 +4,12 @@
 #SBATCH --job-name=lofreqMerged_seq         ### Job Name
 #SBATCH --time=30-0                         ### Wall clock time limit in Days-HH:MM:SS for 30 days - 0 hours
 #SBATCH --cpus-per-task=4                   ### Number of cpus (cores) per task
-#SBATCH --partition=bgmp                    ### partition to run things
+#SBATCH --partition=bgmp                    ### Partition to run things
 
+# ------ Activating conda environment to run lofreq  ------ 
 conda activate /projects/bgmp/shared/groups/2022/79K/salish/envs/lofreq
 
-#lofreq call -f ref.fa -o vars.vcf aln.bam
+#  ------ lofreq call -f ref.fa -o vars.vcf aln.bam  ------ 
 
 /usr/bin/time -v lofreq call -f ../inputs/hg38_formatted_nochr_.fasta -l ../inputs/chris_hg38_reformatted.bed -o vcf.gz /projects/bgmp/shared/groups/2022/79K/salish/kespinoz/proj_salish_cfdna/Quality_Filter/Trimmed_Q25/BWA_25/merged/sorted.merged_Q25.bam
 
